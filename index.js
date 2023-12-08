ddd2e82176efa8f2be85691c30cde6d978d42497
Mohamed Mahmoud <mohamed.mahmoud@ibm.com>
2023-12-08 22:23:58 +0200

    create express server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send(process.env.MY_VAR)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
